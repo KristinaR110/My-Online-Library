2cdb72ebb2f6bafd45ef41f83cbdefced7044834
KristinaR110 <39140459+KristinaR110@users.noreply.github.com>
2018-05-30 19:21:05 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,184 @@
+<?php
+$catalog = [];
+
+$catalog[101] = [
+    "title" => "A Series Of Unfortunate Events: The Bad Beginning",
+    "img" => "img/media/book-cover-1.png",
+   "genre" => "Mystery",
+   "format" => "Paperback",
+   "year" => 1999,
+   "category" => "Books",
+   "authors" => [
+       "Lemony Snicket"
+   ],
+   "publisher" => "HarperCollins",
+   "isbn" => '0-06-440766-7'
+];
+
+$catalog[102] = [
+    "title" => "Mother O' Mine: A Mother's Treasury",
+    "img" => "img/media/book-cover-2.png",
+   "genre" => "Childrens, Poem",
+   "format" => "Hardcover",
+   "year" => 1992,
+   "category" => "Books",
+   "authors" => [
+       "Mary Engelbreit"
+   ],
+   "publisher" => "Andrews McMeel",
+   "isbn" => '978-0740715044'
+];
+
+$catalog[103] = [
+    "title" => "The New Girl",
+    "img" => "img/media/book-cover-3.png",
+   "genre" => "Horror fiction",
+   "format" => "Paperback",
+   "year" => 1989,
+   "category" => "Books",
+   "authors" => [
+       "R.L Stine"
+   ],
+   "publisher" => "Simon Pulse",
+   "isbn" => '978-1416918103'
+];
+
+$catalog[104] = [
+    "title" => "The Giving Tree",
+    "img" => "img/media/book-cover-4.png",
+   "genre" => "Children's picture book",
+   "format" => "Hardcover",
+   "year" => 1964,
+   "category" => "Books",
+   "authors" => [
+       "Shel Silverstein"
+   ],
+   "publisher" => "Harper & Row",
+   "isbn" => '978-0-06-025665-4'
+];
+
+
+
+$catalog[201] = [
+   "title" => "A Silent Voice",
+   "img" => "img/media/movie-cover-1.png",
+   "genre" => "Drama, Romance Novel, Slice Of Life",
+   "format" => "DVD",
+   "year" => 2016,
+   "category" => "Movies",
+   "director" => "Makoto Shinkai",
+   "writers" => [
+       "Reiko Yoshida"    ],
+   "stars" => [
+       "Miyu Irino",
+        "Saori Hayami",
+        "Aoi Yūki",
+        "Kenshō Ono",
+        "Yuki Kaneko",
+        "Yui Ishikawa",
+        "Megumi Han",
+        "Toshiyuki Toyonaga",
+        "Mayu Matsuoka"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "Your Name",
+   "img" => "img/media/movie-cover-2.png",
+   "genre" => "Animation, Romance Film, Drama, Fantasy",
+   "format" => "DVD",
+   "year" => 2016,
+   "category" => "Movies",
+   "director" => "Naoko Yamada",
+   "writers" => [
+       "Makoto Shinkai"    ],
+   "stars" => [
+       "Ryunosuke Kamiki",
+       "Mone Kamishiraishi",
+       "Masami Nagasawa",
+       "Etsuko Ichihara",
+       "Ryo Narita",
+       "Aoi Yūki",
+       "Nobunaga Shimazaki",
+       "Kaito Ishikawa",
+       "Kanon Tani"
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Howl's Moving Castle",
+   "img" => "img/media/movie-cover-3.png",
+   "genre" => "Anime, Animation, Action Film, Adventure Film, Romance Film, Family Film, Drama, Fantasy",
+   "format" => "DVD",
+   "year" => 2004,
+   "category" => "Movies",
+   "director" => "Hayao Miyazaki",
+   "writers" => [
+       "Diana Wynne Jones"    ],
+   "stars" => [
+       "Chieko Baisho",
+       "Takuya Kimura",
+       "Akihiro Miwa"
+   ]
+];
+
+$catalog[204] = [
+   "title" => "The Girl Who Leapt Through Time",
+   "img" => "img/media/movie-cover-4.png",
+   "genre" => "Anime, Animation, Comedy, Romance Film, Science Fiction, Drama",
+   "format" => "DVD",
+   "year" => 2006,
+   "category" => "Movies",
+   "director" => "Mamoru Hosoda",
+   "writers" => [
+       "Satoko Okudera"    ],
+   "stars" => [
+       "Riisa Naka",
+       "Takuya Ishida",
+       "Mitsutaka Itakura",
+       "Sachie Hara"
+   ]
+];
+
+
+$catalog[301] = [
+   "title" => "Twenty One Pilots: Blurryface",
+   "img" => "img/media/music-cover-1.png",
+   "genre" => "Alternative Hip Hop, Eletropop, Indie Pop, Rock",
+   "format" => "CD",
+   "year" => 2015,
+   "category" => "Music",
+   "artist" => "Tyler Joseph, Josh Dun"
+];
+
+$catalog[302] = [
+   "title" => "Panic At The Disoc: Death Of A Bachelor",
+   "img" => "img/media/music-cover-2.png",
+   "genre" => "Pop rock, Pop, Rock, Jazz, Alternative Rock",
+   "format" => "CD",
+   "year" => 2016,
+   "category" => "Music",
+   "artist" => "Brendon Urie, Ian Crawford"
+];
+
+$catalog[303] = [
+   "title" => "Marina And The Diamonds: Electra Heart",
+   "img" => "img/media/music-cover-3.png",
+   "genre" => "Electropop, Dance-pop",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Marina Lambrini Diamandis"
+];
+
+$catalog[304] = [
+   "title" => "Mystery Skulls",
+   "img" => "img/media/music-cover-4.png",
+   "genre" => "Electronica, Indie Pop",
+   "format" => "CD",
+   "year" => 2011,
+   "category" => "Music",
+   "artist" => "Luis Dubuc"
+];
+
+?>
